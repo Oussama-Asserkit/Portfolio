efed2deb3ec194f328359f09aad8df81bf192cd3
Oussama-Asserkit <oussama.asserkit1@gmail.com>
2025-08-30 02:16:46 +0100

    first update project portfolio

---
+++ main.js
@@ -0,0 +1,8 @@
+function ShowMenu(){
+    const sidebar = document.getElementById('mobile__nav')
+    sidebar.style.display = 'flex'
+}
+function CloseMenu(){
+    const sidebar = document.getElementById('mobile__nav')
+    sidebar.style.display = 'none'
+}
